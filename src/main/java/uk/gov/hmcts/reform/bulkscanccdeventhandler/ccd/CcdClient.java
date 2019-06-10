05188fd245477fc81308916b5dc1057beaf594e0
Rafal Kondratowicz <rkondr@gmail.com>
2019-06-10 13:09:25 +0200

    s2s

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/ccd/CcdClient.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/ccd/CcdClient.java
@@ -1,8 +1,15 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler.ccd;
 
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class CcdClient {
+
+    private final Supplier<String> s2sTokenSupplier;
+
+    public CcdClient(Supplier<String> s2sTokenSupplier) {
+        this.s2sTokenSupplier = s2sTokenSupplier;
+    }
 
     public String createCase(Map<String, Object> data, String idamToken) {
         throw new UnsupportedOperationException("Not implemented yet");
