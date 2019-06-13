6a4e63e47be9909a5dc74251c8217a43341e3c94
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-06-13 17:07:15 +0200

    Add ExceptionRecordEventHandlerFactory (#22)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/handler/ExceptionRecordEventHandlerFactory.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.bulkscanccdeventhandler.handler;
+
+import com.google.common.base.Strings;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.ccd.CcdClient;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.ccd.api.CcdApi;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.transformer.ExceptionRecordToCaseTransformer;
+
+import java.util.function.Supplier;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public final class ExceptionRecordEventHandlerFactory {
+
+    private ExceptionRecordEventHandlerFactory() {
+        // util class
+    }
+
+    public static ExceptionRecordEventHandler getHandler(
+        ExceptionRecordToCaseTransformer transformer,
+        String ccdUrl,
+        Supplier<String> s2sTokenSupplier
+    ) {
+        checkNotNull(transformer);
+        checkArgument(!Strings.isNullOrEmpty(ccdUrl));
+        checkNotNull(s2sTokenSupplier);
+
+        return new ExceptionRecordEventHandler(
+            transformer,
+            new CcdClient(
+                Feign.builder()
+                    .decoder(new JacksonDecoder())
+                    .encoder(new JacksonEncoder())
+                    .target(CcdApi.class, ccdUrl),
+                s2sTokenSupplier
+            )
+        );
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/handler/ExceptionRecordEventHandlerFactoryTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.bulkscanccdeventhandler.handler;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.handler.testutils.sampledata.SampleTransformationResult;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.transformer.ExceptionRecordToCaseTransformer;
+
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.handler.ExceptionRecordEventHandlerFactory.getHandler;
+
+@SuppressWarnings("checkstyle:LineLength")
+public class ExceptionRecordEventHandlerFactoryTest {
+
+    private final ExceptionRecordToCaseTransformer transformer = (er) -> SampleTransformationResult.okResult();
+    private final String ccdUrl = "https://localhost:5050";
+    private final Supplier<String> s2sTokenSupplier = () -> UUID.randomUUID().toString();
+
+    @Test
+    public void should_create_an_instance_of_handler() {
+        // when
+        ExceptionRecordEventHandler handler = getHandler(transformer, ccdUrl, s2sTokenSupplier);
+
+        // then
+        assertThat(handler).isNotNull();
+    }
+
+    @Test
+    void should_require_all_arguments_to_be_present() {
+        assertThatThrownBy(() -> getHandler(null, ccdUrl, s2sTokenSupplier)).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> getHandler(transformer, null, s2sTokenSupplier)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> getHandler(transformer, "", s2sTokenSupplier)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> getHandler(transformer, ccdUrl, null)).isInstanceOf(NullPointerException.class);
+    }
+}
