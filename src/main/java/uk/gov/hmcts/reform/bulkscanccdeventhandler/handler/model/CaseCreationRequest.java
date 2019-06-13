0942b2a47f8fce36e42760cc6f6a06647fd2fbe3
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-06-13 12:29:27 +0200

    Include IDAM token in transformation request model (#18)

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/handler/model/CaseCreationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/handler/model/CaseCreationRequest.java
@@ -4,17 +4,22 @@ public class CaseCreationRequest {
 
     public final ExceptionRecord exceptionRecord;
     public final String eventId;
+    public final String idamToken;
     public final boolean ignoreWarnings;
 
     // region constructor
+
     public CaseCreationRequest(
         ExceptionRecord exceptionRecord,
         String eventId,
+        String idamToken,
         boolean ignoreWarnings
     ) {
         this.exceptionRecord = exceptionRecord;
         this.eventId = eventId;
+        this.idamToken = idamToken;
         this.ignoreWarnings = ignoreWarnings;
     }
+
     // endregion
 }
