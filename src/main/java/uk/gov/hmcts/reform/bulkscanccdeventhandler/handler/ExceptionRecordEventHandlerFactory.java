854dc1b8e641073de5fdbf75d9a46e3794918d08
Rafal Kondratowicz <rkondr@gmail.com>
2019-07-08 16:39:42 +0200

    Add more javadoc

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/handler/ExceptionRecordEventHandlerFactory.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/handler/ExceptionRecordEventHandlerFactory.java
@@ -20,6 +20,14 @@ public final class ExceptionRecordEventHandlerFactory {
         // util class
     }
 
+    /**
+     * Creates and instance of ExceptionRecordEventHandler based on parameters provided.
+     *
+     * @param transformer      Transformer that should be used when converting CCD Exception Record to a case.
+     * @param ccdUrl           Base URL to CCD.
+     * @param s2sTokenSupplier function returning your service's s2s token. It will be used when creating a case in CCD.
+     * @return an instance of ExceptionRecordEventHandler
+     */
     public static ExceptionRecordEventHandler getHandler(
         ExceptionRecordToCaseTransformer transformer,
         String ccdUrl,
